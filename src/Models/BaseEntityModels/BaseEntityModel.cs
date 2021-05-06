1b120ef390f3dc0432a63291dd03d38f679d0ed1
GoAnyway <woolftv@mail.ru>
2021-05-06 19:33:27 +0300

    Added base entity models.

---
+++ src/Models/BaseEntityModels/BaseEntityModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Models.BaseEntityModels
+{
+    public abstract class BaseEntityModel : IEntityModel<Guid>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Models/BaseEntityModels/IEntityModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Models.BaseEntityModels
+{
+    public interface IEntityModel<TKey>
+        where TKey : struct, IEquatable<TKey>
+    {
+        TKey Id { get; set; }
+    }
+}
