d8a6d3dfd8957b5d339c0d497795aeefae94f814
GoAnyway <woolftv@mail.ru>
2021-05-06 23:19:34 +0300

    Added API models for Identity controller.

---
+++ src/Models/APIModels/Identity/AuthorizationModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.APIModels.Identity
+{
+    public class AuthorizationModel
+    {
+        [Required] public string Username { get; set; }
+
+        [Required] public string Password { get; set; }
+    }
+}
