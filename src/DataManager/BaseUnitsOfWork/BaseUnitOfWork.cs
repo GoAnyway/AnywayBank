675d34b85c25e39f694e581239053ff1d83d97da
GoAnyway <woolftv@mail.ru>
2021-05-06 20:56:27 +0300

    Added Base units of work.

---
+++ src/DataManager/BaseUnitsOfWork/BaseUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataManager.BaseUnitsOfWork
+{
+    public abstract class BaseUnitOfWork<TDbContext> : IUnitOfWork
+        where TDbContext : DbContext
+    {
+        protected readonly TDbContext Context;
+
+        protected BaseUnitOfWork(TDbContext context)
+        {
+            Context = context;
+        }
+
+        public int SaveChanges() => 
+            Context.SaveChanges();
+
+        public async Task<int> SaveChangesAsync() => 
+            await Context.SaveChangesAsync();
+    }
+}
