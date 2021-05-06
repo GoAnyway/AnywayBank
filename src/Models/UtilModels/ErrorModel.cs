84480acca9ea5614f139865366b725ff668e4007
GoAnyway <woolftv@mail.ru>
2021-05-06 17:57:04 +0300

    Added UtilModels.

---
+++ src/Models/UtilModels/ErrorModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Models.UtilModels
+{
+    public class ErrorModel
+    {
+        public ErrorModel(int code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        public ErrorModel(int code, IEnumerable<string> messages)
+        {
+            Code = code;
+            Message = string.Join(", ", messages);
+        }
+
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ src/Models/UtilModels/ResultModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Models.UtilModels
+{
+    public class ResultModel<TData>
+    {
+        public ResultModel(bool success)
+        {
+            Success = success;
+        }
+
+        public ResultModel(bool success, ErrorModel error)
+        {
+            Success = success;
+            Error = error;
+        }
+
+        public bool Success { get; set; }
+        public ErrorModel Error { get; set; }
+        public TData Data { get; set; }
+    }
+}
