be3eb64d1ccf66fc5dbbf1b737f7d487e093dcee
GoAnyway <woolftv@mail.ru>
2021-05-06 18:44:40 +0300

    Added DbContext registration to DI container.

--- src/AnywayBank/Startup.cs
+++ src/AnywayBank/Startup.cs
@@ -1,3 +1,4 @@
+using AnywayBank.Utils.Extensions.ServiceCollectionExtensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,7 +19,9 @@ namespace AnywayBank
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) =>
-            services.AddControllers();
+            services
+                .AddDbContexts(Configuration)
+                .AddControllers();
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
---
+++ src/AnywayBank/Utils/Extensions/ServiceCollectionExtensions/DbContextsServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Database.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AnywayBank.Utils.Extensions.ServiceCollectionExtensions
+{
+    public static class DbContextsServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDbContexts(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<BankDbContext>(builder => 
+                builder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+}
