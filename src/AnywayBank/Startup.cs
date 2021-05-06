08b456ca78f6d771b149cd9f63f4f62c2ef811da
GoAnyway <woolftv@mail.ru>
2021-05-06 22:49:28 +0300

    Added Core services registration to DI container.

--- src/AnywayBank/Startup.cs
+++ src/AnywayBank/Startup.cs
@@ -24,6 +24,7 @@ namespace AnywayBank
                 .AddRepositories()
                 .AddUnitsOfWork()
                 .AddAutoMapper()
+                .AddCoreServices()
                 .AddControllers();
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/AnywayBank/Utils/Extensions/ServiceCollectionExtensions/CoreServicesServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using AnywayBankCore.Services.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AnywayBank.Utils.Extensions.ServiceCollectionExtensions
+{
+    public static class CoreServicesServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services) =>
+            services
+                .AddScoped<IIdentityService, IdentityService>();
+    }
+}
