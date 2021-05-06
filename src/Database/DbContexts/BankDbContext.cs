2f37e4668699687662e5fa7dce2331c33b3121aa
GoAnyway <woolftv@mail.ru>
2021-05-06 17:39:41 +0300

    Added EntityFramework nuget-packages, BankDbContext and DbSet's for identity and bank entities.

---
+++ src/Database/DbContexts/BankDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Database.Entities.Core.Bank;
+using Database.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.DbContexts
+{
+    public sealed class BankDbContext : DbContext
+    {
+        public BankDbContext(DbContextOptions<BankDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<BankProfile> BankProfiles { get; set; }
+    }
+}
