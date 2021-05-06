96b20a33250645c20638172d3e894a56cf5e6cbd
GoAnyway <woolftv@mail.ru>
2021-05-06 21:31:28 +0300

    Added Base services.

---
+++ src/AnywayBankCore/BaseServices/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DataManager.UnitsOfWork.AnywayBankUnitsOfWork;
+
+namespace AnywayBankCore.BaseServices
+{
+    public abstract class BaseService : BaseService<IAnywayBankUnitOfWork>
+    {
+        protected BaseService(IAnywayBankUnitOfWork unitOfWork, IMapper mapper) 
+            : base(unitOfWork, mapper)
+        {
+        }
+    }
+
+    public abstract class BaseService<TUnitOfWork>
+    {
+        protected readonly TUnitOfWork UnitOfWork;
+        protected readonly IMapper Mapper;
+
+        protected BaseService(TUnitOfWork unitOfWork, IMapper mapper)
+        {
+            UnitOfWork = unitOfWork;
+            Mapper = mapper;
+        }
+    }
+}
