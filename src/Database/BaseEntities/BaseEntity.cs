ee121a0b93a0d3612359b753b6b02b07624808ad
GoAnyway <woolftv@mail.ru>
2021-05-06 16:26:46 +0300

    Added Base entities to Database project.

---
+++ src/Database/BaseEntities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Database.BaseEntities
+{
+    public abstract class BaseEntity : IEntity<Guid>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Database/BaseEntities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Database.BaseEntities
+{
+    public interface IEntity<TKey>
+        where TKey : struct, IEquatable<TKey>
+    {
+        TKey Id { get; set; }
+    }
+}
