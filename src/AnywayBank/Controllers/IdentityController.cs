41eec759b9b59dc345f76d645b3060b4af0310ec
GoAnyway <woolftv@mail.ru>
2021-05-07 14:20:18 +0300

    Added Identity controller.

---
+++ src/AnywayBank/Controllers/IdentityController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using AnywayBankCore.Cores;
+using Microsoft.AspNetCore.Mvc;
+using Models.APIModels.Identity;
+
+namespace AnywayBank.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly ICore _core;
+
+        public IdentityController(ICore core)
+        {
+            _core = core;
+        }
+
+        [HttpPost]
+        [Route(nameof(Register))]
+        public async Task<IActionResult> Register(RegistrationModel model)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            
+            var result = await _core.IdentityService.RegisterAsync(model);
+            
+            return result.Success
+                ? new ObjectResult(result.Data)
+                : BadRequest(result.Error);
+        }
+
+        [HttpPost]
+        [Route(nameof(Authorize))]
+        public async Task<IActionResult> Authorize(AuthorizationModel model)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            var result = await _core.IdentityService.AuthorizeAsync(model);
+
+            return result.Success
+                ? new ObjectResult(result.Data)
+                : StatusCode(result.Error.Code, result.Error.Message);
+        }
+    }
+}
